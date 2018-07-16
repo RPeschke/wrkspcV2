06ffbee3e387469ba599b4fc8cd774116ea3798b
Chris Ketter <cketter@hawaii.edu>
2018-07-16 11:04:14 -1000

    Created .cc file from "extract_peak.hh"

--- KLM_lib/KLM_lib/include/KLM_lib/extract_peak.hh
+++ KLM_lib/KLM_lib/include/KLM_lib/extract_peak.hh
@@ -4,26 +4,7 @@
 #include <vector>
 
 
-inline feature extract_peak(const std::vector<int>& ADC_counts) {
-
-
-	feature max_element;
-
-
-	for (size_t i = 0; i < ADC_counts.size(); i++)
-	{
-		if (ADC_counts[i] > max_element.signal) {
-
-			max_element.signal = ADC_counts[i];
-			max_element.time = i;
-
-		}
-
-	}
-
-	return max_element;
-}
-
+feature extract_peak(const std::vector<int>& ADC_counts);
 
 
 
