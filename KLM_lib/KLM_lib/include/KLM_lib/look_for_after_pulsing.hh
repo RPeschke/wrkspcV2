01653b3f87d753c361813646e024841aeb39c949
Chris Ketter <cketter@hawaii.edu>
2018-07-16 12:20:55 -1000

    added function to look for after pulses in waveforms

---
+++ KLM_lib/KLM_lib/include/KLM_lib/look_for_after_pulsing.hh
@@ -0,0 +1,11 @@
+#ifndef look_for_after_pulsing_h__
+#define look_for_after_pulsing_h__
+#include "feature.hpp"
+#include <vector>
+
+
+feature look_for_after_pulsing(const std::vector<int>& ADC_counts);
+
+
+
+#endif // look_for_after_pulsing_h__
