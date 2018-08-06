d44dc569fcf13559102a9d268e3461b3a5b74f41
Chris Ketter <cketter@hawaii.edu>
2018-08-06 10:06:34 -1000

    added more functions to anal_lib.py for waveform plotting and single-photon-spectra fitting

--- lib/anal_lib.py
+++ lib/anal_lib.py
@@ -21,13 +21,27 @@ class NewAnalysis:
         self.SN                 = str(sys.argv[1])
         self.strHV              = str(sys.argv[2])
         self.rawHV              = float(self.strHV.replace("p","."))
-
+        self.FeatExtd_data = self.run.root_file.replace('.root','_features.root')
+        self.HV_DAC = run.Get_ASIC_HV_from_file(0,'quiet')
+        self.PlotsDir = run.PlotsDir
         #--- Load Root Macros---#
-        ROOT.gROOT.LoadMacro("root/Feature_extraction/ExtractFeatures_ck.cxx")
-
-    def ProcessWaveforms(self, infile="", outfile=""):
-        '''const char* root_input, const char* root_output'''
+        ROOT.gROOT.LoadMacro("root/Feature_extraction/ProcessWaveforms.cxx")
+        ROOT.gROOT.LoadMacro("root/GainStudies/MultiGaussFit.cxx")
+        ROOT.gROOT.LoadMacro("root/WaveformPlotting/StreamAnnotatedSingleChWaveformsToCanvas.cxx")
+    def ProcessWaveforms(self, infile=""):
         if not (infile): infile = self.run.root_file
-        if not (outfile): outfile = infile.replace('.root','_features.root')
-        if (self.DEBUG): print("Infile: %s,  Outfile: %s" % (infile,outfile))
-        ROOT.ProcessWaveformData(infile,outfile)
+        if (self.DEBUG): print("Infile: %s" % (infile))
+        ROOT.ProcessWaveformData(infile)
+
+    def FitSinglePhotonSpectra(self, CH, infile=""):
+        approxHV = self.rawHV - 5*(self.HV_DAC[CH] + self.run.HVDAC_offset[CH])/256.
+        if not (infile): infile = self.run.root_file
+        if (self.DEBUG): print("Infile: %s" % (infile))
+        ''' const char* root_file, const char* PlotsDir, const int   argCH, const float approxHV) '''
+        ROOT.MultiGaussFit(infile, self.PlotsDir, CH, approxHV)
+
+    def StreamWaveformsToTerminal(self, infile=""):
+        if not (infile): infile = self.run.root_file
+        if (self.DEBUG): print("Infile: %s, CH: %d, start:%d, stop: %d, dispTime: %d" % (infile))
+        ''' const char* root_file, const char* PlotsDir, const int   argCH, const float approxHV) '''
+        ROOT.PlotFeatures(infile)
